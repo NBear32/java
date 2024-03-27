f39eb10db1ff8dbb8801605f16efde6dddc709b4
NBear32 <hansel32153@gmail.com>
2024-03-27 17:22:12 +0900

    20240327

---
+++ studyjava/src/Java20240327/TryCatchFinally.java
@@ -0,0 +1,29 @@
+package Java20240327;
+
+public class TryCatchFinally {
+    public static void main(String[] args) {
+        try {
+            System.out.println(3/0);
+            int a = Integer.parseInt("20");
+            int[] arr = {1,2,3,4,5};
+            arr[5] = 6; // ArrayIndexOutOfBoundsException 발생
+            System.out.println("프로그램 종료1");
+        }catch (ArithmeticException e) {
+            System.out.println("숫자는 0으로 나눌수 없습니다");
+            System.out.println("프로그램 종료2");
+        }catch (NumberFormatException e) {
+            System.out.println("숫자로 변환할 수 없습니다");
+        }catch (NullPointerException e) {
+            System.out.println("Null 관리 좀 잘하자");
+        }catch (Exception e) {
+            System.out.println("예기치못한 에러로 인하여 프로그램을 종료합니다");
+        }
+        finally {
+            System.out.println("프로그램 종료3");
+        }
+        //System.out.println("여기가 실행될까?");
+        // try 밖에 실행코드를 넣는것은 좋지 않음.
+        // 이 위치는 예외가 발생해도 실행이 되기 때문에 실행코드의 의미가 없고
+        // 정상의 경우라면 finally 안에서 실행코드를 마무리하는 것이 좋음
+    }
+}
---
+++ studyjava/src/Java20240327Exam/BinarySearchExample.java
@@ -0,0 +1,27 @@
+package Java20240327Exam;
+
+public class BinarySearchExample {
+    public static void binarySearch(int arr[], int first, int last, int key){ // first 시작, last 끝, key 찾을 값
+        int mid = (first + last)/2;   // mid 에 배열의 중간 값을 넣음
+        while( first <= last ){    // first 가 last 보다 같거나 작을때까지
+            if ( arr[mid] < key ){   // mid 가 key 보다 작으면 first 는 mid + 1
+                first = mid + 1;
+            }else if ( arr[mid] == key ){   // mid 가 key 이면 바로 출력하고 break
+                System.out.println("Element is found at index: " + mid);
+                break;
+            }else{
+                last = mid - 1;   // mid 가 last 보다 크면 last 는 mid - 1
+            }
+            mid = (first + last)/2;   // mid 에 first 와 last 의 중간값을 넣고 다시 돌림
+        }
+        if ( first > last ){   // first 가 last 보다 커질때까지 값을 못찾으면 출력
+            System.out.println("Element is not found!");
+        }
+    }
+    public static void main(String args[]){
+        int arr[] = {10,20,30,40,50};
+        int key = 30;
+        int last=arr.length-1;
+        binarySearch(arr,0,last,key);
+    }
+}
